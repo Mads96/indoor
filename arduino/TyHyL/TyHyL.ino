47fdfe787a62892a3ba706597ab457aa1f694aeb
mads96 <mads.contacto@gmail.com>
2015-04-17 05:55:40 -0300

    too

---
+++ arduino/TyHyL/TyHyL.ino
@@ -0,0 +1,57 @@
+
+#include "DHT.h"
+
+#define DHTPIN 2     // Pin del Arduino al cual esta conectado el pin 2 del sensor
+
+// Descomentar segun el tipo de sensor DHT usado
+//#define DHTTYPE DHT11   // DHT 11 
+#define DHTTYPE DHT22   // DHT 22  (AM2302)
+//#define DHTTYPE DHT21   // DHT 21 (AM2301)
+
+// Diagrama
+// Pin 1 Sensor a +5V de Arduino
+// Pin 2 Sensor a HDTPIN (en este sketch es el pin 2)
+// Pin 4 Sensor a GROUND de  Arduino
+// Resistencia de 10K desde Pin 2 de sensor a Pin 1 de Sensor o +5V
+int l=0;
+int m=0;
+// Inicializa el sensor
+DHT dht(DHTPIN, DHTTYPE);
+
+// Configura Arduino 
+void setup() {
+  Serial.begin(9600); 
+  Serial.println("InternetdelasCosas.cl\n\nPrueba de sensor DHTxx!");
+ 
+  dht.begin();
+}
+
+void loop() {
+  // Espera dos segundos para realizar la primera medición.
+  delay(2000);
+
+  // Lee los datos entregados por el sensor, cada lectura demora 250 milisegundos
+  // El sensor muestrea la temperatura cada 2 segundos}
+  // Obtiene la Humedad
+  float h = dht.readHumidity();
+  // Obtiene la Temperatura en Celsius
+  float t = dht.readTemperature();
+  
+  m=analogRead (0);
+  // Control de errores, valida que se obtuvieron valores para los datos medidos
+  if (isnan(h) || isnan(t)) {
+    Serial.println("Falla al leer el sensor DHT!");
+    return;
+  }
+
+  Serial.print("Humedad: "); 
+  Serial.print(h);
+  Serial.println(" %\t");
+  Serial.print("Temperatura: "); 
+  Serial.print(t);
+  Serial.println(" *C ");
+  Serial.print("LUZ: "); 
+  Serial.print(m);
+  Serial.println(" brillo ");
+  Serial.println();
+}
---
+++ arduino/indoor_sin_comentarios/indoor_sin_comentarios.ino
@@ -0,0 +1,42 @@
+
+#include "DHT.h"
+DHT dht1(2,DHT22); 
+DHT dht2(3,DHT22); 
+int l=0;
+int lx=0;
+void setup() {
+  Serial.begin(9600); 
+  Serial.println("MADS96.cl\n\nIN DOOR!");
+  Serial.println();
+  dht1.begin();
+}
+
+void loop() {
+
+  delay(2000);
+  float h = dht1.readHumidity();
+  float t = dht1.readTemperature();
+  float h1 = dht2.readHumidity();
+  float t1 = dht2.readTemperature();
+  lx=analogRead (0);
+  if (isnan(h) || isnan(t)) {
+    Serial.println("Falla al leer el sensor DHT!");
+    return;
+  }
+  Serial.print("Humedad: "); 
+  Serial.print(h);
+  Serial.println(" %\t");
+  Serial.print("Temperatura: "); 
+  Serial.print(t);
+  Serial.println(" *C ");
+   Serial.print("Humedad: "); 
+  Serial.print(h1);
+  Serial.println(" %\t");
+  Serial.print("Temperatura: "); 
+  Serial.print(t1);
+  Serial.println(" *C ");
+  Serial.print("LUZ: "); 
+  Serial.print(lx);
+  Serial.println(" brillo ");
+  Serial.println();
+}
